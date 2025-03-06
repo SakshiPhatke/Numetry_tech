568247bea0f30216004a49c1c42e104162fdb963
SakshiPhatke <sakshiphatke@gmail.com>
2025-03-06 18:10:20 +0530

    Added Second Task folder to repository

---
+++ Numetry_tech/Home page/src/Components/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            order now and satisfy your cravings! 
+          </h1>
+          <p className="primary-text">
+          we are dedicated to serving you the freshest and most delicious meals. 
+          From sizzling appetizers to hearty main courses and delightful desserts, 
+          our menu is crafted to satisfy every craving.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
